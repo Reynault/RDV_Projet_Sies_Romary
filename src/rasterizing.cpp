dce958be9290a87bcc1de73e8a6816af2b8bacd5
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2020-02-10 00:32:58 +0100

    We can now display a mesh like diablo using simple orthographic projection.

--- src/rasterizing.cpp
+++ src/rasterizing.cpp
@@ -15,15 +15,15 @@ bool inRange(int x, int y, int width, int height){
  *  drawing a line using bresenham algorithm
  */
 void line(int x1, int y1, int x2, int y2, std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
-    if(inRange(x1, y1, width, height) && inRange(x2, y2, width, height)){
+    if(inRange(x1, y1, width, height) && inRange(x2, y2, width, height)) {
         int dx, dy, error, derror;
         bool steep = false;
-        if(std::abs(x2 - x1) < std::abs(y2 - y1)){
+        if (std::abs(x2 - x1) < std::abs(y2 - y1)) {
             std::swap(x1, y1);
             std::swap(x2, y2);
             steep = true;
         }
-        if(x1 > x2){
+        if (x1 > x2) {
             std::swap(x1, x2);
             std::swap(y1, y2);
         }
@@ -32,15 +32,15 @@ void line(int x1, int y1, int x2, int y2, std::vector<Vec3f> &framebuffer, int w
         derror = std::abs(dy) * 2;
         error = 0;
         int y = y1;
-        for(int x = x1; x <= x2; x++){
-            if(steep){
-                framebuffer[y+x*width] = color;
-            }else{
-                framebuffer[x+y*width] = color;
+        for (int x = x1; x <= x2; x++) {
+            if (steep) {
+                framebuffer[y + x * width] = color;
+            } else {
+                framebuffer[x + y * width] = color;
             }
-            if((error = error + derror) > dx){
-                y += (y1> y2) ? -1 : 1;
-                error -= dx*2;
+            if ((error = error + derror) > dx) {
+                y += (y1 > y2) ? -1 : 1;
+                error -= dx * 2;
             }
         }
     }
@@ -65,7 +65,7 @@ void polygonMesh(int x[], int y[], int nbLines, std::vector<Vec3f> &framebuffer,
  */
 void writeInFile(std::vector<Vec3f> &framebuffer, int width, int height){
     std::ofstream ofs;
-    ofs.open("../images/out2.ppm", std::ios::binary);
+    ofs.open("../images/out3.ppm", std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for(size_t i = 0; i < height*width; i++){
         Vec3f &c = framebuffer[i];
@@ -79,24 +79,32 @@ void writeInFile(std::vector<Vec3f> &framebuffer, int width, int height){
 }
 
 void render(){
-    const Vec3f black = Vec3f(0, 0, 0);
-    const Vec3f red = Vec3f(1, 0, 0);
-    const Vec3f green = Vec3f(0, 1, 0);
-    const Vec3f blue = Vec3f(0, 0, 1);
     const Vec3f white = Vec3f(1, 1, 1);
+    const int fov = 90;
     const int width = 1024;
     const int height = 768;
+    Model diablo = Model("../res/diablo3_pose.obj");
     std::vector<Vec3f> framebuffer(width*height);
+    Vec3f camera = Vec3f(0, 0, 0);
+    Vec3f orient = Vec3f(0, 0, 0);
 
     // drawing things
-    int x[3] = {400, 600, 600};
-    int y[3] = {400, 600, 400};
-    polygonMesh(x, y, 3, framebuffer, width, height, white);
+    int nbPoints = 3;
+    int x[nbPoints], y[nbPoints];
+    Vec3f point;
+    for(int i = 0; i < diablo.nfaces(); i++){
+        for(int j = 0; j < nbPoints; j++){
+            point = diablo.point(diablo.vert(i, j));
+            x[j] = width - (int)((point.x+1) * width)/2;
+            y[j] = height - (int)((point.y+1) * height)/2;
+        }
+        polygonMesh(x, y, nbPoints, framebuffer, width, height, white);
+    }
+
     // generating file
     writeInFile(framebuffer, width, height);
 }
 
 int main() {
-    std::cout << "Rendering images" << std::endl;
     render();
 }
