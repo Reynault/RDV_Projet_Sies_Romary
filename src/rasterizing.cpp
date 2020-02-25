5eeee4163f74b36bc6d74fe50eb29995864a0b52
Sies Reynault <reynault.sies8@etu.univ-lorraine.fr>
2020-02-25 17:25:10 +0100

    We can now display a diablo with triangles using sweeping lines algorithm.

--- src/rasterizing.cpp
+++ src/rasterizing.cpp
@@ -1,5 +1,4 @@
-#include <iostream>
-#include "model.cpp"
+#include "rasterizing.h"
 /*
  * Doc used for the development of this project: https://github.com/ssloy/tinyrenderer/wiki/Lesson-0-getting-started
  */
@@ -14,9 +13,9 @@ bool inRange(int x, int y, int width, int height){
 /*
  *  drawing a line using bresenham algorithm
  */
-void line(int x1, int y1, int x2, int y2, std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
+void drawLine(int x1, int y1, int x2, int y2, std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
     if(inRange(x1, y1, width, height) && inRange(x2, y2, width, height)) {
-        int dx, dy, error, derror;
+        int dx, dy, error, offset, shift, relocate;
         bool steep = false;
         if (std::abs(x2 - x1) < std::abs(y2 - y1)) {
             std::swap(x1, y1);
@@ -29,43 +28,136 @@ void line(int x1, int y1, int x2, int y2, std::vector<Vec3f> &framebuffer, int w
         }
         dx = x2 - x1;
         dy = y2 - y1;
-        derror = std::abs(dy) * 2;
+        offset = std::abs(dy) * 2;
         error = 0;
+        relocate = dx * 2;
+        shift = (y1 > y2) ? -1 : 1;
         int y = y1;
         for (int x = x1; x <= x2; x++) {
-            if (steep) {
-                framebuffer[y + x * width] = color;
-            } else {
-                framebuffer[x + y * width] = color;
-            }
-            if ((error = error + derror) > dx) {
-                y += (y1 > y2) ? -1 : 1;
-                error -= dx * 2;
+            (steep) ? framebuffer[y + x * width] = color : framebuffer[x + y * width] = color;
+            if ((error = error + offset) > dx) {
+                y += shift;
+                error -= relocate;
             }
         }
     }
 }
 
 /*
- * drawing a triangle as a mesh
+ * drawing a polygon as a mesh
  */
-void polygonMesh(int x[], int y[], int nbLines, std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
-    for(int i = 0; i < nbLines-1; i++){
-        line(x[i], y[i], x[i+1], y[i+1],
+void drawPolygonMesh(Vec3f points[], int nbLines, std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
+    for(int i = 0; i < nbLines-1; i++){ // drawing every lines
+        drawLine(points[i].x, points[i].y, points[i+1].x, points[i+1].y,
                 framebuffer, width, height, color);
     }
-    if(nbLines > 1){
-        line(x[0], y[0], x[nbLines-1], y[nbLines-1], framebuffer, width, height, color);
+    if(nbLines > 1){    // drawing last one
+        drawLine(points[0].x, points[0].y, points[nbLines-1].x, points[nbLines-1].y,
+                framebuffer, width, height, color);
     }
 }
 
+
+float computeTriangleAire(Vec3f points[]){
+    return (
+        (points[0].x * std::abs(points[1].y - points[2].y)) +
+        (points[1].x * std::abs(points[0].y - points[2].y)) +
+        (points[2].x * std::abs(points[0].y - points[1].y))
+    )/2.0f;
+}
+
+/*
+ * drawing a triangle using line sweeping algorithm (first approach)
+ */
+void sweepingTriangle(Vec3f points[], std::vector<float> &zBuffer,
+        std::vector<Vec3f> &framebuffer, int width, int height, Vec3f color){
+    Vec3f A, B, C, AB, BC, AC, pAC, pAB, pBC;
+    A = points[0];
+    B = points[1];
+    C = points[2];
+
+    // sort vertices of the triangle by their y-coordinates;
+    if(A.y > B.y) std::swap(A, B);
+    if(B.y > C.y) std::swap(B, C);
+    if(A.y > B.y) std::swap(A, B);
+
+    // getting vectors AB, BC, AC
+    AB = Vec3f(
+            std::abs(B.x - A.x),
+            std::abs(B.y - A.y),
+            std::abs(B.z - A.z)
+    );
+    BC = Vec3f(
+            std::abs(B.x - C.x),
+            std::abs(B.y - C.y),
+            std::abs(B.z - C.z)
+    );
+    AC = Vec3f(
+            std::abs(C.x - A.x),
+            std::abs(C.y - A.y),
+            std::abs(C.z - A.z)
+    );
+
+    // getting points of the current line
+    pAC = A;
+    pAB = A;
+    pBC = B;
+
+    // drawing upper part of the triangle
+    if(AB.y > 0){
+        for (float i = 0; i < AB.y; i++) {
+            // linear interpolation to retrieve x position of pAC
+            // (depending on the direction of the segment)
+            (A.x < C.x ) ? pAC.x = A.x + (AC.x * i) / AC.y : pAC.x = A.x - (AC.x * i) / AC.y ;
+            pAC.y = A.y + i;
+
+            (A.x < B.x) ? pAB.x = A.x + (AB.x * i) / AB.y : pAB.x = A.x - (AB.x * i) / AB.y;
+            pAB.y = pAC.y;
+
+            drawLine(pAB.x, pAB.y, pAC.x, pAC.y, framebuffer, width, height, color);
+        }
+    }else{
+        drawLine(A.x, A.y, B.x, B.y, framebuffer, width, height, color);
+    }
+
+
+    // drawing lower part of the triangle
+    if(BC.y > 0){
+        for(float i = AB.y; i < AC.y; i++){
+            (A.x < C.x ) ? pAC.x = A.x + (AC.x * i) / AC.y : pAC.x = A.x - (AC.x * i) / AC.y ;
+            pAC.y = A.y + i;
+
+            (B.x < C.x ) ? pBC.x = B.x + (BC.x * (i-AB.y)) / BC.y : pBC.x = B.x - (BC.x * (i-AB.y)) / BC.y;
+            pBC.y = pAC.y;
+
+            drawLine(pBC.x, pBC.y, pAC.x, pAC.y, framebuffer, width, height, color);
+        }
+    }else{
+        drawLine(B.x, B.y, C.x, C.y, framebuffer, width, height, color);
+    }
+
+    drawLine(A.x, A.y, B.x, B.y, framebuffer, width, height, color);
+    drawLine(A.x, A.y, C.x, C.y, framebuffer, width, height, color);
+    drawLine(B.x, B.y, C.x, C.y, framebuffer, width, height, color);
+
+}
+
+bool updateZBuffer(float x, float y, Vec3f points[], std::vector<float> &zBuffer,
+        std::vector<Vec3f> &framebuffer, int width, int height){
+    bool res = false;
+    if(inRange(x, y, width, height)){
+        // bi-linear interpolation to retrieve Z
+        // checking if z in z-buffer is <
+    }
+    return res;
+}
 
 /*
  * writing a ppm image, code from: https://github.com/ssloy/tinyraytracer
  */
 void writeInFile(std::vector<Vec3f> &framebuffer, int width, int height){
     std::ofstream ofs;
-    ofs.open("../images/out3.ppm", std::ios::binary);
+    ofs.open("../images/out4.ppm", std::ios::binary);
     ofs << "P6\n" << width << " " << height << "\n255\n";
     for(size_t i = 0; i < height*width; i++){
         Vec3f &c = framebuffer[i];
@@ -79,27 +171,37 @@ void writeInFile(std::vector<Vec3f> &framebuffer, int width, int height){
 }
 
 void render(){
-    const Vec3f white = Vec3f(1, 1, 1);
     const int fov = 90;
     const int width = 1024;
     const int height = 768;
     Model diablo = Model("../res/diablo3_pose.obj");
     std::vector<Vec3f> framebuffer(width*height);
+    std::vector<float> zBuffer(width*height);
     Vec3f camera = Vec3f(0, 0, 0);
     Vec3f orient = Vec3f(0, 0, 0);
 
-    // drawing things
+    // drawing things here
     int nbPoints = 3;
-    int x[nbPoints], y[nbPoints];
+    Vec3f points[nbPoints];
     Vec3f point;
-    for(int i = 0; i < diablo.nfaces(); i++){
+    #pragma omp parallel for
+    for(int i = 0; i < diablo.nfaces(); i++){   // fetching diablo's mesh
         for(int j = 0; j < nbPoints; j++){
             point = diablo.point(diablo.vert(i, j));
-            x[j] = width - (int)((point.x+1) * width)/2;
-            y[j] = height - (int)((point.y+1) * height)/2;
+            point.x = width - ((point.x+1) * width)/2;
+            point.y = height - ((point.y+1) * height)/2;
+            points[j] = point;
         }
-        polygonMesh(x, y, nbPoints, framebuffer, width, height, white);
+        sweepingTriangle(points, zBuffer, framebuffer, width, height, WHITE);
     }
+
+    Vec3f t1[] = {Vec3f(10,70,10), Vec3f(50,160,10), Vec3f(70,80,20)};
+    Vec3f t2[]  = {Vec3f(180,50,15), Vec3f(150,1,15), Vec3f(70,180,15)};
+    Vec3f t3[]  = {Vec3f(180,150,0), Vec3f(120,160,0), Vec3f(130,180,60)};
+
+    sweepingTriangle(t1, zBuffer, framebuffer, width, height, RED);
+    sweepingTriangle(t2, zBuffer, framebuffer, width, height, GREEN);
+    sweepingTriangle(t3, zBuffer, framebuffer, width, height, BLUE);
 
     // generating file
     writeInFile(framebuffer, width, height);
---
+++ src/rasterizing.h
@@ -0,0 +1,16 @@
+//
+// Created by Reynault on 10/02/2020.
+//
+
+#ifndef RDV_PROJET_SIES_ROMARY_RASTERIZING_H
+#define RDV_PROJET_SIES_ROMARY_RASTERIZING_H
+
+#include <iostream>
+#include "model.cpp"
+const Vec3f WHITE = Vec3f(1, 1, 1);
+const Vec3f BLACK = Vec3f(0, 0, 0);
+const Vec3f RED = Vec3f(1, 0, 0);
+const Vec3f GREEN = Vec3f(0, 1, 0);
+const Vec3f BLUE = Vec3f(0, 0, 1);
+
+#endif //RDV_PROJET_SIES_ROMARY_RASTERIZING_H
